d6ed970ee0e21f6a9c8fa3d6690fb1f0ca9ffdf9
Yilmaz Mustafa <dev@mail.be>
2023-05-29 02:04:57 +0200

    [STREAM] app run fixes.

--- src/stream/__init__.py
+++ src/stream/__init__.py
@@ -33,7 +33,11 @@ def index():
     return render_template('index.html')
 
 def main():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(
+        host='localhost', 
+        port=8080,
+        debug=True
+    )
 
 if __name__ == '__main__':
     main()
