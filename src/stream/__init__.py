184d09445da36d8f6764831a140efb5421b62d93
Yilmaz Mustafa <dev@mail.be>
2023-05-29 02:11:36 +0200

    render is loaded from string instead of an html page.

--- src/stream/__init__.py
+++ src/stream/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, render_template_string
 import cv2
 
 app = Flask(__name__)
@@ -7,6 +7,7 @@ camera = cv2.VideoCapture(0)  # use 0 for web camera
 camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 camera.set(cv2.CAP_PROP_FPS, 30)
+
 
 def gen_frames():  # generate frame by frame from camera
     while True:
@@ -18,7 +19,7 @@ def gen_frames():  # generate frame by frame from camera
             _, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
 
 @app.route('/video_feed')
@@ -30,16 +31,43 @@ def video_feed():
 @app.route('/')
 def index():
     """Video streaming home page."""
-    return render_template(
-        "https://raw.githubusercontent.com/codesapienbe/aij/main/src/stream/templates/index.html"
+    return render_template_string(
+        """
+        <!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>AIJ Live Stream</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8  offset-lg-2">
+            <h3 class="mt-5">Live Streaming...</h3>
+            <img src="{{ url_for('video_feed') }}" width="100%">
+        </div>
+    </div>
+</div>
+</body>
+</html>
+"""
     )
+
 
 def main():
     app.run(
-        host='localhost', 
+        host='localhost',
         port=8080,
         debug=True
     )
+
 
 if __name__ == '__main__':
     main()
